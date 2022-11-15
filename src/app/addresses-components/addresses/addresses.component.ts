1a35bd25de52d8773b0ba363a167e07987cebc36
Pieter B <pieterbeernink@hotmail.com>
2022-11-15 23:14:57 +0100

    create addresses list

--- src/app/addresses-components/addresses/addresses.component.ts
+++ src/app/addresses-components/addresses/addresses.component.ts
@@ -1,26 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Address } from 'src/app/models/api-models/address.model';
 import { AddressService } from '../address.service';
 
 @Component({
   selector: 'app-addresses',
   templateUrl: './addresses.component.html',
-  styleUrls: ['./addresses.component.scss']
+  styleUrls: ['./addresses.component.scss'],
 })
 export class AddressesComponent implements OnInit {
+  addresses: Address[] = []; // UI model
 
-  constructor(private _addressService: AddressService) { }
+  // List columns for Angular materialize table
+  displayedColumns: string[] = [
+    'addressType',
+    'description',
+    'streetName',
+    'streetNumber',
+    'postalCode',
+    'city',    
+  ];
+
+  // Create datasource for Angular materialize table
+  dataSource: MatTableDataSource<Address> = new MatTableDataSource<Address>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+
+  filterString = '';
+
+  constructor(private _addressService: AddressService) {}
 
   ngOnInit(): void {
-    // Fetch students
+    // Fetch addresses
     this._addressService.getAddresses().subscribe(
-      (success) => {
-        console.log('Addresses fetched');
-        console.log(success);
+      (successRepsonse) => {
+        this.addresses = successRepsonse;
+        this.dataSource = new MatTableDataSource<Address>(this.addresses); // fill dataSource with list of addresses
+
+        if (this.matPaginator) {
+          this.dataSource.paginator = this.matPaginator;
+        }
+
+        if (this.matSort) {
+          this.dataSource.sort = this.matSort;
+        }
       },
       (errorResponse) => {
-        console.log(errorResponse);
+        // console.log(errorResponse);
       }
-    ); 
+    );
   }
 
+  filterAddresses() {
+    this.dataSource.filter = this.filterString.trim().toLowerCase();
+  }
 }
