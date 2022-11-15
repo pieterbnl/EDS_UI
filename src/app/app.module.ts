57aee7e0d649bcc32ed51040bfbcd5d411e6aac6
Pieter B <pieterbeernink@hotmail.com>
2022-11-15 21:54:22 +0100

    clear up app and add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,11 +51,12 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 // Material Data tables
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
+import { TopNavComponent } from './top-nav/top-nav.component'
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
